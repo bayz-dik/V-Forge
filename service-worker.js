8ede0da1d0da6c5a22c0b219bd78869610c1cd1a
bayz-dik <bayuandk4@gmail.com>
2026-08-04 06:32:21 +0700

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,13 +1,14 @@
 // V-Forge Service Worker
 // Ganti angka versi ini tiap kali ada update besar, biar cache lama dibuang otomatis
-const CACHE_VERSION = 'vforge-v3-1-profile-sync-v1';
+const CACHE_VERSION = 'vforge-v4-0-project-history-v1';
 
 const ASSETS_TO_CACHE = [
   './index.html',
-  './css/style.css?v=3.1.0',
-  './js/firebase-config.js?v=3.1.0',
-  './js/auth.js?v=3.1.0',
-  './js/app.js?v=3.1.0',
+  './css/style.css?v=4.0.0',
+  './js/firebase-config.js?v=4.0.0',
+  './js/app.js?v=4.0.0',
+  './js/projects.js?v=4.0.0',
+  './js/auth.js?v=4.0.0',
   './manifest.json',
   './icons/icon-192.png',
   './icons/icon-512.png'
